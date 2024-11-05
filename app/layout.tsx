1c7e0ff8ebb7647695c3c99ef9b2ff963d028d19
jeronimofsneto <jeronimo@ufdpar.edu.br>
2024-11-05 16:45:14 -0300

    feat: add login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Layout principale</h1>
         {children}
       </body>
     </html>
